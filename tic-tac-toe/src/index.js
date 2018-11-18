67bc2be0ffaa408a00c57e1a022cacf56e9b7f21
cglotr <amir.ariffin.920404@gmail.com>
2018-11-18 12:36:03 +0800

    moves

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -81,6 +81,16 @@ class Game extends React.Component {
       status = `Next player: ${current.xIsNext ? 'X' : 'O'}`;
     }
 
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #${move}` : 'Go to game start';
+
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     return (
       <div className="game">
         <div className="game-board">
@@ -91,7 +101,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
