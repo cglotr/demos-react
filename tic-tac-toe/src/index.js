8c0ee68586f1e0c1381a549c5cd603a9d5f43857
cglotr <amir.ariffin.920404@gmail.com>
2018-11-18 13:00:09 +0800

    jump to history

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -45,12 +45,13 @@ class Game extends React.Component {
           squares: Array(9).fill(null),
         },
       ],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -63,13 +64,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const squares = current.squares;
 
     let status;
@@ -78,10 +87,10 @@ class Game extends React.Component {
     if (winner) {
       status = `Winner: ${winner}`;
     } else {
-      status = `Next player: ${current.xIsNext ? 'X' : 'O'}`;
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
     }
 
-    const moves = history.map((step, move) => {
+    const moves = history.map((_, move) => {
       const desc = move ? `Go to move #${move}` : 'Go to game start';
 
       return (
